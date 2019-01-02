c1fe6595d84b5a2ae040b18d4e8aad09261870e9
Eli Cleveland <elicleveland12@gmail.com>
2019-01-02 15:36:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(array)
     i = 0 
     new_array = []
     while i < array.length 
-      new_array.push(yield array[i])
+      new_array.push(yield array[i].upcase)
       i = i + 1 
     end 
     new_array 

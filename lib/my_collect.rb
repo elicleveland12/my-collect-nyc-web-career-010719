aea8890fda296c39456cf2f44a93f047ee3ee911
Eli Cleveland <elicleveland12@gmail.com>
2019-01-02 15:33:17 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,13 @@
 def my_collect(array)
   if block_given?
     i = 0 
+    new_array = []
     while i < array.length 
       yield array[i]
+      new_array << i.upcase
       i = i + 1 
     end 
-    array 
+    new_array
   else 
     puts "No block was given!"
   end 

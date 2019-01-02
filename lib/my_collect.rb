e334a4dbbecf699d54af4e752ebd89ee49e04003
Eli Cleveland <elicleveland12@gmail.com>
2019-01-02 15:31:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,8 @@
 def my_collect(array)
   if block_given?
     i = 0 
-    new_array = []
     while i < array.length 
       yield array[i]
-      new_array << i.upcase
-      array = new_array
       i = i + 1 
     end 
     array 
